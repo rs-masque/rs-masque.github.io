feac6554193b1c82201d1455e039bf23337a6842
rs-masque <rs.masque@gmail.com>
2017-07-31 01:27:08 +0300

    commit

--- script.js
+++ script.js
@@ -1,41 +1,30 @@
 function makeGrid(x, y) {
     var grid = new Array(x);
-    for (var i = 0; i < y; i++) {
+    for (var i = 0; i < x; i++) {
         grid[i] = new Array(y);
-        for (var j=0;j<y;j++) grid[i][j]=0;
+        for (var j=0; j<y ;j++) grid[i][j]=0;
     }
     return grid;
 }
 
-// function cloneGrid(oldGrid) {
-//     var newGrid = new Array(oldGrid.length);
-//     for (var i=0; i < oldGrid.length; i++){
-//         newGrid[i] = new Array(oldGrid[i].length);
-//         newGrid[i] = oldGrid[i].slice(0);
-//     }
-//     return newGrid;
-// }
-
-function randGrid(grid) {
-    var temp = 0;
-    for (var i=0; i<grid.length;i++){
-        for (var j=0; j<grid[i].length;j++) {
-            grid[i][j] = Math.floor(Math.random() * (3));
-            temp++;
-            if (temp > 60){
-                return grid;
+function randGrid(grid, car_N) {
+    for (var i=0; i < car_N;){
+        var tx = Math.floor(Math.random() * (grid.length));
+        var ty = Math.floor(Math.random() * (grid[0].length));
+        if (grid[tx][ty] == 0) {
+            grid[tx][ty] = Math.floor(Math.random() * 2 + 1);
+            i++;
             }
-        }
     }
     return grid;
 }
 
 function drawGrid(x,y) {
     var hT = "<table cellpadding='0' cellspacing='1'>";
-    for (var j = 0; j < y; j++) {
+    for (var i = 0; i < x; i++) {
         hT += "<tr>";
-        for (var i = 0; i < x; i++) {
-            hT += "<td id='cell"+j+i+"'>";
+        for (var j = 0; j < y; j++){
+            hT += "<td id='cell"+i+j+"'>";
             hT += "</td>";
         }
         hT += "</tr>";
@@ -64,13 +53,16 @@ function showCars(grid) {
 function isSafe(grid,a,b) {
     return (grid[a][b] == 0);
 }
+function isBlue(grid,a,b) {
+    return (grid[a][b] % 2);
+}
 
 function moveCars(grid) {
     for (var j = 0; j < grid[0].length; j++) {
         for (var i = 0; i < grid.length; i++) {
             if (grid[i][j] == 2 || grid[i][j] == 4) {
-                if (i == 0 && isSafe(grid, y - 1, j)) {
-                    grid[y - 1][j] = 6;
+                if (i == 0 && isSafe(grid, grid.length - 1, j)) {
+                    grid[grid.length - 1][j] = 6;
                     grid[i][j] = 0;
                 }
                 else if (i > 0 && isSafe(grid, i - 1, j)) {
@@ -87,13 +79,21 @@ function moveCars(grid) {
         }
     }
     for (var i = 0; i < grid.length; i++) {
-        for (var j = grid[i].length; j >= 0; j--) {
+        for (var j = grid[0].length; j >= 0; j--) {
             if (grid[i][j] == 1 || grid[i][j] == 3) {
-                if (j == x - 1 && isSafe(grid, i, 0)) {
+                if (i == grid.length-1 && j == grid[0].length -1 && isSafe(grid, i, 0) && (isSafe(grid, 0, 0)|| isBlue(grid,0,0))){
                     grid[i][0] = 5;
                     grid[i][j] = 0;
                 }
-                else if (j < x - 1 && isSafe(grid, i, j + 1)) {
+                else if (j == grid[0].length - 1 && isSafe(grid, i, 0) && (isSafe(grid, i + 1, 0) || isBlue(grid, i+1,0))) {
+                    grid[i][0] = 5;
+                    grid[i][j] = 0;
+                }
+                else if (i == grid.length - 1 && isSafe(grid, i, j + 1) && (isSafe(grid, 0, j + 1) || isBlue(grid, 0, j+1))) {
+                    grid[i][j + 1] = 1;
+                    grid[i][j] = 0;
+                }
+                else if (i < grid.length - 1 && j < grid[0].length - 1 && isSafe(grid, i, j + 1) && (isSafe(grid, i + 1, j + 1) || isBlue(grid, i+1,j+1))) {
                     grid[i][j + 1] = 1;
                     grid[i][j] = 0;
                 }
@@ -109,13 +109,18 @@ function moveCars(grid) {
     return grid;
 }
 
+var x, y, prior, car_N;
+var grid;
 
-var x=10
-var y=10
-
-drawGrid(x,y);
-var grid  = randGrid(makeGrid(x,y));
-showCars(grid);
+document.getElementById('Submit_Par').onclick = function() {
+    x = parseInt($('#Set_X').val());
+    y = parseInt($('#Set_Y').val());
+    car_N = parseInt($('#Car_N').val());
+    prior = parseFloat($('#verPrior'));
+    grid = randGrid(makeGrid(x,y), car_N);
+    drawGrid(x,y);
+    showCars(grid);
+}
 
 document.getElementById('Tick').onclick = function() {
     grid = moveCars(grid);
