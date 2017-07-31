1c0f25ea6b06c940975474927029043569229415
rs-masque <rs.masque@gmail.com>
2017-07-31 03:32:23 +0300

    123

--- script.js
+++ script.js
@@ -81,16 +81,16 @@ function moveCars(grid) {
     for (var i = 0; i < grid.length; i++) {
         for (var j = grid[0].length - 1; j >= 0; j--) {
             if (grid[i][j] == 1 || grid[i][j] == 3) {
-                if (i == grid.length-1 && j == grid[0].length -1 && isSafe(grid, i, 0) && (isSafe(grid, 0, 0)|| isBlue(grid,0,0))){
+                if (i == grid.length - 1 && j == grid[0].length - 1 && isSafe(grid, i, 0) && (isSafe(grid, 0, 0) || isBlue(grid,0,0))){
+                    grid[i][0] = 5;
+                    grid[i][j] = 0;
+                }
+                else if (i != grid.length - 1 && j == grid[0].length - 1 && isSafe(grid, i, 0) && (isSafe(grid, i + 1, 0) || isBlue(grid, i+1,0))) {
                     grid[i][0] = 5;
                     grid[i][j] = 0;
                 }
                 else if (i == grid.length - 1 && isSafe(grid, i, j + 1) && (isSafe(grid, 0, j + 1) || isBlue(grid, 0, j+1))) {
                     grid[i][j + 1] = 1;
-                    grid[i][j] = 0;
-                }
-                else if (j == grid[0].length - 1 && isSafe(grid, i, 0) && (isSafe(grid, i + 1, 0) || isBlue(grid, i+1,0))) {
-                    grid[i][0] = 5;
                     grid[i][j] = 0;
                 }
                 else if (i < grid.length - 1 && j < grid[0].length - 1 && isSafe(grid, i, j + 1) && (isSafe(grid, i + 1, j + 1) || isBlue(grid, i+1,j+1))) {
